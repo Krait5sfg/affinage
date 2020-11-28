7a1c4d0aaca8cdd9ca889c7b718839745675b0b2
Yura <yurii_kulachenko@mail.ru>
2020-11-28 12:43:08 +0300

    Слайдер для мобилки

--- source/js/script.js
+++ source/js/script.js
@@ -1,73 +1,62 @@
 'use strict';
 
 //слайдер
-const LEFT_LIMIT_SCROLL = 160; // самая крайняя левая граница скрола слайдера
-
-const slider = document.querySelector(`.slider__list`);
-let activeElement = document.querySelector(`.slider__item--active`);
-let number = +activeElement.dataset.number;
-
-const slideTitle = document.querySelector(`.slider__image-title`);
-const slideCurrentNumberElement = document.querySelector(`.slider__current-number`);
-const overallNumber = document.querySelectorAll(`.slider__item`).length;
-document.querySelector(`.slider__overall-number`).textContent = ` ${overallNumber}`;
-
-setSlideTitle();
-setSlideNumber();
-
-slider.scrollLeft = number === 1 ? LEFT_LIMIT_SCROLL : LEFT_LIMIT_SCROLL + ((number - 1) * 220);
-console.log(slider.scrollLeft);
-// slider.scrollLeft = LEFT_LIMIT_SCROLL;
-
 let start;
 let change;
+let offset = 0;
 
-slider.addEventListener('touchstart', (evt) => {
-  start = evt.touches[0].clientX; // координаты прикосновения
+const sliderElements = document.querySelectorAll(`.slider__item`);
+const limitRightOffset = (sliderElements.length - 1) * 220;
+const sliderListElement = document.querySelector(`.slider__list`);
+const sliderImageTitleElement = document.querySelector(`.slider__image-title`);
+let sliderItemActive = sliderListElement.querySelector(`.slider__item--active`);
+let numberActiveSlider = sliderItemActive.dataset.number;
+const sliderCurrentNumberElement = document.querySelector(`.slider__current-number`);
+const sliderOverallNumberElement = document.querySelector(`.slider__overall-number`);
+
+sliderImageTitleElement.textContent = sliderItemActive.dataset.title;
+sliderCurrentNumberElement.textContent = `${sliderItemActive.dataset.number} `;
+sliderOverallNumberElement.textContent = `/ ${sliderElements.length}`;
+
+sliderListElement.addEventListener('touchstart', (evt) => {
+  start = evt.touches[0].clientX;
 });
 
-slider.addEventListener('touchmove', (evt) => {
+sliderListElement.addEventListener('touchmove', (evt) => {
   evt.preventDefault();
-  change = start - evt.touches[0].clientX; //evt.touches[0].clientX координаты где палец оторвался от экрана
+  let touch = evt.touches[0];
+  change = start - touch.clientX;
 });
 
-slider.addEventListener('touchend', (evt) => {
-  activeElement.classList.remove('slider__item--active');
+sliderListElement.addEventListener('touchend', slideShow);
+
+function slideShow() {
 
   if (change > 0) {
-    try {
-      slider.scrollLeft += 220;
-      number++;
-      if (slider.querySelector(`[data-number="${number}"]`)) { }
-      activeElement = slider.querySelector(`[data-number="${number}"]`);
-      activeElement.classList.add(`slider__item--active`);
-    } catch (error) {
-      number--;
-      slider.scrollLeft -= LEFT_LIMIT_SCROLL;
-      activeElement = slider.querySelector(`[data-number="${number}"]`);
-      activeElement.classList.add(`slider__item--active`);
+
+    if (Math.abs(offset) === limitRightOffset) {
+      return false;
     }
+
+    numberActiveSlider++;
+    sliderItemActive = sliderListElement.querySelector(`[data-number="${numberActiveSlider}"]`);
+    sliderImageTitleElement.textContent = sliderItemActive.dataset.title;
+    sliderCurrentNumberElement.textContent = `${sliderItemActive.dataset.number} `;
+
+    offset -= 220;
+    sliderListElement.style.left = `${offset}px`;
   } else {
-    try {
-      number--;
-      slider.scrollLeft -= 220;
-      activeElement = slider.querySelector(`[data-number="${number}"]`);
-      activeElement.classList.add(`slider__item--active`);
-    } catch (error) {
-      number++;
-      slider.scrollLeft += LEFT_LIMIT_SCROLL;
-      activeElement = slider.querySelector(`[data-number="${number}"]`);
-      activeElement.classList.add(`slider__item--active`);
+
+    if (offset === 0) {
+      return false;
     }
+
+    numberActiveSlider--;
+    sliderItemActive = sliderListElement.querySelector(`[data-number="${numberActiveSlider}"]`);
+    sliderImageTitleElement.textContent = sliderItemActive.dataset.title;
+    sliderCurrentNumberElement.textContent = `${sliderItemActive.dataset.number} `;
+
+    offset += 220;
+    sliderListElement.style.left = `${offset}px`;
   }
-  setSlideTitle();
-  setSlideNumber();
-});
-
-function setSlideTitle() {
-  slideTitle.textContent = activeElement.dataset.description;
-}
-
-function setSlideNumber() {
-  slideCurrentNumberElement.textContent = `${activeElement.dataset.number} /`;
-}
+};
